843cac6a5ea7ac0e9d68465146861fbdd455fcc3
Yash Changrani <changraniyash@gmail.com>
2018-08-30 19:51:31 +0530

    Initial commit

---
+++ app/src/main/java/com/patientapp/patientinformationsystem/LoginTest.java
@@ -0,0 +1,9 @@
+
+package com.patientapp.patientinformationsystem;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+public class LoginTest {
+    public static void main(String args[]) {
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+    }
+}
